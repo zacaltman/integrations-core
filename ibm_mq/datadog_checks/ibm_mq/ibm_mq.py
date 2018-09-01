165f29117c8f84ce5e263314d6c7fe766edc8ec7
Greg Meyer <greg.meyer@datadoghq.com>
2018-09-01 17:42:42 -0400

    linux only, some other updates

--- ibm_mq/datadog_checks/ibm_mq/ibm_mq.py
+++ ibm_mq/datadog_checks/ibm_mq/ibm_mq.py
@@ -1,9 +1,11 @@
 # (C) Datadog, Inc. 2018
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+
 from datadog_checks.checks import AgentCheck
 
 
 class IbmMqCheck(AgentCheck):
+
     def check(self, instance):
         pass
