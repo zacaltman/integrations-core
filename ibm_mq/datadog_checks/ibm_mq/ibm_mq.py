0ed6cf5ae7bfb3ba35c8dc6645b66f87b0b4cda0
Greg Meyer <greg.meyer@datadoghq.com>
2018-09-05 18:03:35 -0400

    first pass

--- ibm_mq/datadog_checks/ibm_mq/ibm_mq.py
+++ ibm_mq/datadog_checks/ibm_mq/ibm_mq.py
@@ -4,8 +4,80 @@
 
 from datadog_checks.checks import AgentCheck
 
+from ast import literal_eval
+
+try:
+    import pymqi
+except ImportError:
+    pymqi = None
+
+from . import errors, metrics
+
 
 class IbmMqCheck(AgentCheck):
 
+    METRIC_PREFIX = 'ibm_mq'
+
     def check(self, instance):
-        pass
+        if not pymqi:
+            self.log.error("You need to install pymqi")
+            raise errors.PymqiException("You need to install pymqi")
+
+        channel = instance.get('channel')
+        queue_manager_name = instance.get('queue_manager', 'default')
+
+        host = instance.get('host')
+        port = instance.get('port')
+
+        host_and_port = "{}({})".format(host, port)
+
+        username = instance.get('username')
+        password = instance.get('password')
+
+        custom_tags = instance.get('tags', [])
+
+        tags = [
+            "queue_manager:{}".format(queue_manager_name),
+            "host:{}".format(host),
+            "port:{}".format(port)
+        ]
+
+        tags += custom_tags
+
+        if username and password:
+            queue_manager = pymqi.connect(queue_manager_name, channel, host_and_port, user, password)
+        else:
+            queue_manager = pymqi.connect(queue_manager_name, channel, host_and_port)
+
+        self.queue_manager_stats(queue_manager, tags)
+
+        queues = instance.get('queues', [])
+
+        for queue_name in queues:
+            queue_tags = tags + ["queue:{}".format(queue_name)]
+            try:
+                queue = pymqi.Queue(queue_manager, queue_name)
+                self.queue_stats(queue_manager, queue, queue_tags)
+            except Exception as e:
+                self.log.warning('Cannot connect to queue {}: {}'.format(queue_name, e))
+
+    def queue_manager_stats(self, queue_manager, tags):
+        for mname, value in metrics.QUEUE_MANAGER_METRICS:
+            try:
+                arg = 'qmgr.inquire(pymqi.CMQC.{})'.format(value)
+                m = literal_eval(arg)
+
+                mname = '{}.queue_manager.{}'.format(METRIC_PREFIX, mname)
+                self.gauge(mname, m, tags=tags)
+            except pyaci.Error as e:
+                self.log.info("Error getting queue manager stats: {}".format(e))
+
+    def queue_stats(self, queue_manager, queue, tags):
+        for mname, value in metrics.QUEUE_METRICS:
+            try:
+                arg = 'queue.inquire(pymqi.CMQC.{})'.format(value)
+                m = literal_eval(arg)
+                mname = '{}.queue.{}'.format(METRIC_PREFIX, mname)
+                self.gauge(mname, m, tags=tags)
+            except pyaci.Error as e:
+                self.log.info("Error getting queue manager stats: {}".format(e))
--- ibm_mq/datadog_checks/ibm_mq/metrics.py
+++ ibm_mq/datadog_checks/ibm_mq/metrics.py
@@ -1,13 +1,15 @@
+# (C) Datadog, Inc. 2018
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+
 QUEUE_METRICS = {
     'QServiceInterval': 'MQIA_Q_SERVICE_INTERVAL',
     'Shareability': 'MQIA_SHAREABILITY',
     'InhibitPut': 'MQIA_INHIBIT_PUT',
     'QDepthLowLimit': 'MQIA_Q_DEPTH_LOW_LIMIT',
     'InhibitGet': 'MQIA_INHIBIT_GET',
-    'CreationTime': 'MQCA_CREATION_TIME',
     'HardenGetBackout': 'MQIA_HARDEN_GET_BACKOUT',
     'QServiceIntervalEvent': 'MQIA_Q_SERVICE_INTERVAL_EVENT',
-    'QName': 'MQCA_Q_NAME',
     'InitiationQName': 'MQCA_INITIATION_Q_NAME',
     'TriggerControl': 'MQIA_TRIGGER_CONTROL',
     'Usage': 'MQIA_USAGE',
@@ -21,25 +23,18 @@ QUEUE_METRICS = {
 	'QDepthLowEvent': 'MQIA_Q_DEPTH_LOW_EVENT',
 	'TriggerMsgPriority': 'MQIA_TRIGGER_MSG_PRIORITY',
 	'CurrentQDepth': 'MQIA_CURRENT_Q_DEPTH',
-	'ClusterNamelist': 'MQCA_CLUSTER_NAMELIST',
 	'QDepthMaxEvent': 'MQIA_Q_DEPTH_MAX_EVENT',
 	'OpenInputCount': 'MQIA_OPEN_INPUT_COUNT',
 	'DefPersistence': 'MQIA_DEF_PERSISTENCE',
-	'DistLists': 'MQIA_DIST_LISTS',
 	'TriggerDepth': 'MQIA_TRIGGER_DEPTH',
 	'MaxMsgLength': 'MQIA_MAX_MSG_LENGTH',
 	'QDepthHighLimit': 'MQIA_Q_DEPTH_HIGH_LIMIT',
-	'ProcessName': 'MQCA_PROCESS_NAME',
-	'AlterationTime': 'MQCA_ALTERATION_TIME',
 	'DefPriority': 'MQIA_DEF_PRIORITY',
 	'DefInputOpenOption': 'MQIA_DEF_INPUT_OPEN_OPTION',
 	'MsgDeliverySequence': 'MQIA_MSG_DELIVERY_SEQUENCE',
-	'CreationDate': 'MQCA_CREATION_DATE',
 	'DefBind': 'MQIA_DEF_BIND',
 	'ClusterName': 'MQCA_CLUSTER_NAME',
 	'RetentionInterval': 'MQIA_RETENTION_INTERVAL',
-	'AlterationDate': 'MQCA_ALTERATION_DATE',
-	'BackoutRequeueName': 'MQCA_BACKOUT_REQ_Q_NAME',
 	'OpenOutputCount': 'MQIA_OPEN_OUTPUT_COUNT',
 	'TriggerType': 'MQIA_TRIGGER_TYPE',
 	'QDesc': 'MQCA_Q_DESC'
@@ -56,4 +51,10 @@ QUEUE_MANAGER_METRICS = {
 
 QUEUE_MANAGER_TAGS = {
 	'QMgrIdentifier': 'MQCA_Q_MGR_IDENTIFIER',
+	'ProcessName': 'MQCA_PROCESS_NAME',
+}
+
+CREATION = {
+    'CreationTime': 'MQCA_CREATION_TIME',
+	'CreationDate': 'MQCA_CREATION_DATE',
 }
