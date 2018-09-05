68510634c5f721aab40789d02c3baaf104354552
Greg Meyer <greg.meyer@datadoghq.com>
2018-09-05 13:28:26 -0400

    adds some metrics

---
+++ ibm_mq/datadog_checks/ibm_mq/metrics.py
@@ -0,0 +1,59 @@
+QUEUE_METRICS = {
+    'QServiceInterval': 'MQIA_Q_SERVICE_INTERVAL',
+    'Shareability': 'MQIA_SHAREABILITY',
+    'InhibitPut': 'MQIA_INHIBIT_PUT',
+    'QDepthLowLimit': 'MQIA_Q_DEPTH_LOW_LIMIT',
+    'InhibitGet': 'MQIA_INHIBIT_GET',
+    'CreationTime': 'MQCA_CREATION_TIME',
+    'HardenGetBackout': 'MQIA_HARDEN_GET_BACKOUT',
+    'QServiceIntervalEvent': 'MQIA_Q_SERVICE_INTERVAL_EVENT',
+    'QName': 'MQCA_Q_NAME',
+    'InitiationQName': 'MQCA_INITIATION_Q_NAME',
+    'TriggerControl': 'MQIA_TRIGGER_CONTROL',
+    'Usage': 'MQIA_USAGE',
+    'Scope': 'MQIA_SCOPE',
+    'QType': 'MQIA_Q_TYPE',
+    'MaxQDepth': 'MQIA_MAX_Q_DEPTH',
+    'BackoutThreshold': 'MQIA_BACKOUT_THRESHOLD',
+	'TriggerData': 'MQCA_TRIGGER_DATA',
+	'QDepthHighEvent': 'MQIA_Q_DEPTH_HIGH_EVENT',
+	'DefinitionType': 'MQIA_DEFINITION_TYPE',
+	'QDepthLowEvent': 'MQIA_Q_DEPTH_LOW_EVENT',
+	'TriggerMsgPriority': 'MQIA_TRIGGER_MSG_PRIORITY',
+	'CurrentQDepth': 'MQIA_CURRENT_Q_DEPTH',
+	'ClusterNamelist': 'MQCA_CLUSTER_NAMELIST',
+	'QDepthMaxEvent': 'MQIA_Q_DEPTH_MAX_EVENT',
+	'OpenInputCount': 'MQIA_OPEN_INPUT_COUNT',
+	'DefPersistence': 'MQIA_DEF_PERSISTENCE',
+	'DistLists': 'MQIA_DIST_LISTS',
+	'TriggerDepth': 'MQIA_TRIGGER_DEPTH',
+	'MaxMsgLength': 'MQIA_MAX_MSG_LENGTH',
+	'QDepthHighLimit': 'MQIA_Q_DEPTH_HIGH_LIMIT',
+	'ProcessName': 'MQCA_PROCESS_NAME',
+	'AlterationTime': 'MQCA_ALTERATION_TIME',
+	'DefPriority': 'MQIA_DEF_PRIORITY',
+	'DefInputOpenOption': 'MQIA_DEF_INPUT_OPEN_OPTION',
+	'MsgDeliverySequence': 'MQIA_MSG_DELIVERY_SEQUENCE',
+	'CreationDate': 'MQCA_CREATION_DATE',
+	'DefBind': 'MQIA_DEF_BIND',
+	'ClusterName': 'MQCA_CLUSTER_NAME',
+	'RetentionInterval': 'MQIA_RETENTION_INTERVAL',
+	'AlterationDate': 'MQCA_ALTERATION_DATE',
+	'BackoutRequeueName': 'MQCA_BACKOUT_REQ_Q_NAME',
+	'OpenOutputCount': 'MQIA_OPEN_OUTPUT_COUNT',
+	'TriggerType': 'MQIA_TRIGGER_TYPE',
+	'QDesc': 'MQCA_Q_DESC'
+}
+
+QUEUE_TAGS = {
+
+}
+
+QUEUE_MANAGER_METRICS = {
+    'DistLists': 'MQIA_DIST_LISTS',
+	'MaxMsgLength': 'MQIA_MAX_MSG_LENGTH',
+}
+
+QUEUE_MANAGER_TAGS = {
+	'QMgrIdentifier': 'MQCA_Q_MGR_IDENTIFIER',
+}
